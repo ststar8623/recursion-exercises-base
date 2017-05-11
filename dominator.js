31eff4f6aba7c9913728ef5391ca93b2667870b9
ststar8623 <ststar8623@gmail.com>
2017-05-11 14:47:44 -0700

    dominator.js

--- dominator.js
+++ dominator.js
@@ -3,14 +3,41 @@ const _ = require('underscore');
 
 const getElementById = function(root, id) {
   // Your code here
+  let arr = flattenTreeToArray(root);
+  let res;
+  _.each(arr, el=>{
+    if(el.id === id){
+      res = el;
+    }
+  })
+  return res;
 };
 
 const getElementsByClassName = function(root, className) {
   // Your code here
+  let arr = flattenTreeToArray(root);
+  let res = [];
+  _.each(arr, el=>{
+    for(let name in el.classList){
+      if(el.classList[name] === className){
+        res.push(el);
+      }
+    }
+  });
+  return res;
+  
 };
 
 const getElementsByTagName = function(root, tagName) {
   // Your code here
+  let arr  = flattenTreeToArray(root);
+  let res = [];
+  _.each(arr, el=>{
+    if(el.tagName === tagName){
+      res.push(el);
+    }
+  });
+  return res;
 };
 
 module.exports = {
