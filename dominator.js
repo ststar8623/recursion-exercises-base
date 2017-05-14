5a6dd1d14b329083ca10803bd72b2f96d21316a5
ststar8623 <ststar8623@gmail.com>
2017-05-14 16:45:33 -0700

    fix dominator

--- dominator.js
+++ dominator.js
@@ -3,41 +3,22 @@ const _ = require('underscore');
 
 const getElementById = function(root, id) {
   // Your code here
-  let arr = flattenTreeToArray(root);
-  let res;
-  _.each(arr, el=>{
-    if(el.id === id){
-      res = el;
-    }
-  })
-  return res;
+  const nodes = flattenTreeToArray(root);
+  const matches = _.filter(nodes, node => node.id === id);
+  return matches.length > 0 ? matches[0] : null;
 };
 
 const getElementsByClassName = function(root, className) {
   // Your code here
-  let arr = flattenTreeToArray(root);
-  let res = [];
-  _.each(arr, el=>{
-    for(let name in el.classList){
-      if(el.classList[name] === className){
-        res.push(el);
-      }
-    }
-  });
-  return res;
+  const nodes = flattenTreeToArray(root);
+  return _.filter(nodes, node => (node.className || '').indexOf(className) !== -1);
   
 };
 
 const getElementsByTagName = function(root, tagName) {
   // Your code here
-  let arr  = flattenTreeToArray(root);
-  let res = [];
-  _.each(arr, el=>{
-    if(el.tagName === tagName){
-      res.push(el);
-    }
-  });
-  return res;
+  const nodes = flattenTreeToArray(root);
+  return _.filter(nodes, node => (node.tagName || '').toUpperCase() === tagName.toUpperCase());
 };
 
 module.exports = {
